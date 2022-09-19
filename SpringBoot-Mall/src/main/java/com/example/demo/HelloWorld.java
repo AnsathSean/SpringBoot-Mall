0a3e741be778284fd18b0d286739abb977e4c4ad
chsu23 <chsu23@US-5CG01050N1>
2022-09-19 17:19:36 +0800

    新增Hello Springboot Java

---
+++ SpringBoot-Mall/src/main/java/com/example/demo/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+	
+	@RequestMapping("/")
+	public String hello(){
+		return "Hey, Spring Boot 的 Hello World ! ";
+	}
+}
