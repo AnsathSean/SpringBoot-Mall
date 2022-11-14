beaca7204f327aeb82e56a968e27f0366fcd43cb
chsu23 <chsu23@US-5CG01050N1>
2022-11-14 17:50:24 +0800

    新增排序功能

--- SpringBoot-Mall/src/main/java/com/EricHsu/springbootmall/controller/ProductController.java
+++ SpringBoot-Mall/src/main/java/com/EricHsu/springbootmall/controller/ProductController.java
@@ -29,12 +29,19 @@ public class ProductController {
 	//取得商品清單
 	@GetMapping("/products")
 	public ResponseEntity<List<Product>> getProducts(
+			//查詢條件 Filtering
 			@RequestParam(required = false) ProductCategory category,
-			@RequestParam(required = false) String search
+			@RequestParam(required = false) String search,
+			
+			//排序 Sorting
+			@RequestParam(defaultValue = "created_date") String orderBy,
+			@RequestParam(defaultValue = "desc") String sort
 	){
 		ProductQueryParams productQueryParams = new ProductQueryParams();
 		productQueryParams.setCategory(category);
 		productQueryParams.setSearch(search);
+		productQueryParams.setOrderBy(orderBy);
+		productQueryParams.setSort(sort);
 		
 		List<Product> proudctList = productServie.getProducts(productQueryParams);
 		
