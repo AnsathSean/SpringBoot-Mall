a8c57b9e7ff0dd79ddc507409e92ee6bbac03386
chsu23 <chsu23@US-5CG01050N1>
2022-12-06 17:42:14 +0800

    新增分頁功能

--- SpringBoot-Mall/src/main/java/com/EricHsu/springbootmall/controller/ProductController.java
+++ SpringBoot-Mall/src/main/java/com/EricHsu/springbootmall/controller/ProductController.java
@@ -3,9 +3,13 @@ package com.EricHsu.springbootmall.controller;
 
 import java.util.List;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +25,7 @@ import com.EricHsu.springbootmall.dto.ProductRequest;
 import com.EricHsu.springbootmall.model.Product;
 import com.EricHsu.springbootmall.service.ProductServie;
 
+@Validated
 @RestController
 public class ProductController {
 	
@@ -35,13 +40,19 @@ public class ProductController {
 			
 			//排序 Sorting
 			@RequestParam(defaultValue = "created_date") String orderBy,
-			@RequestParam(defaultValue = "desc") String sort
+			@RequestParam(defaultValue = "desc") String sort,
+			
+			//分頁 Pagenation
+			@RequestParam(defaultValue = "5")  @Max(1000) @Min(0) Integer limit,
+			@RequestParam(defaultValue = "0") @Min(0) Integer offset
 	){
 		ProductQueryParams productQueryParams = new ProductQueryParams();
 		productQueryParams.setCategory(category);
 		productQueryParams.setSearch(search);
 		productQueryParams.setOrderBy(orderBy);
 		productQueryParams.setSort(sort);
+		productQueryParams.setLimit(limit);
+		productQueryParams.setOffset(offset);
 		
 		List<Product> proudctList = productServie.getProducts(productQueryParams);
 		
